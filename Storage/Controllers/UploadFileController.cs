ac4d90c8e2359d82b12da7e5d527bd93e4c95104
dehghan.a <azadeh_dehghan1@yahoo.com>
2025-10-21 18:02:29 +0100

    Add function for uploading a file by KeyVault.

--- Storage/Controllers/UploadFileController.cs
+++ Storage/Controllers/UploadFileController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace Storage.Controllers
 {
@@ -23,6 +25,41 @@ namespace Storage.Controllers
             string connectionString = "YOUR STORAGE ACCOUNT CONNECTION";
             if (string.IsNullOrWhiteSpace(connectionString))
                 return StatusCode(500, "Storage connection string is missing. Set AZURE_STORAGE_CONNECTION_STRING.");
+
+            // 2- Client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            string containername = "test" + Guid.NewGuid().ToString();
+            var containerClient = blobServiceClient.GetBlobContainerClient(containername);
+
+            // Create container if it doesn't exist (idempotent)
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.None);
+
+            // 3) Target blob
+            var blobClient = containerClient.GetBlobClient(file.FileName);
+
+            // 4) Upload stream (overwrite true if you want to replace same-name files)
+            await using var stream = file.OpenReadStream();
+            await blobClient.UploadAsync(stream, new BlobUploadOptions{HttpHeaders = new BlobHttpHeaders{ContentType = file.ContentType}});
+
+            // Optional: return blob URL
+            return Ok(new
+            {
+                file = file.FileName,
+                url = blobClient.Uri.ToString()
+            });
+        }
+
+        [HttpPost("upload-with-keyvault")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> UploadFileWithKeyVault(IFormFile file)
+        {
+            if (file is null || file.Length == 0)
+                return BadRequest("No file is uploaded.");
+
+            // 1- Create connection by key-vault
+            var client = new SecretClient(new Uri("https://virakeyvaulttest.vault.azure.net/"), new DefaultAzureCredential());
+            KeyVaultSecret secret = await client.GetSecretAsync("StorageConnectionString");
+            string connectionString = secret.Value;
 
             // 2- Client
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
